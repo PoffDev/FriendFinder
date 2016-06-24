924a80dc23104d2c221b34ad3eeb67a75c8f8250
PoffDev <joshuapoffenberger@gmail.com>
2016-06-24 16:37:16 -0400

    added place holders for api in js, added friends array

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		"name": "Rapunzel",
+		"image": "Images/Rapunzel.jpg",
+		"scores": [5, 5, 1, 1, 4, 4, 1, 4, 5, 3]
+	},
+	{
+		"name": "Anna",
+		"image": "Images/Anna.jpg",
+		"scores": [5, 4, 5, 5, 1, 4, 2, 5, 5, 5]
+	},
+	{
+		"name": "Aurora",
+		"image": "Images/Aurora.jpg",
+		"scores": [1, 3, 3, 1, 5, 5, 1, 1, 1, 5]
+	},
+	{
+		"name": "Cinderella",
+		"image": "Images/Cindi.jpg",
+		"scores": [1, 2, 1, 1, 2, 4, 5, 5, 5, 5]
+	},
+	{
+		"name": "Jasmine",
+		"image": "Images/Jasmine.jpg",
+		"scores": [4, 2, 1, 4, 3, 4, 5, 2, 5, 5]
+	},
+];
+
+//export
